90e5c1a5753edf56d71e5dd7d5a40a648ba91fc2
Bingjian(Alex) Huang <huangbj16@mails.tsinghua.edu.cn>
2019-05-02 20:58:10 +0800

    reform lstm to optimization

--- lstm-classification/data.py
+++ lstm-classification/data.py
@@ -7,43 +7,67 @@ import os
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 from scipy.fftpack import fft,ifft
+from python_audio_feature import mfcc
 
 def findFiles(path): return glob.glob(path)
 
 # Read a file and split into lines
 def read_data(filename):
     data = np.load(filename)
-#     print(data.shape)
+    # print(data.shape)
     return data
 
 # Build the category_lines dictionary, a list of lines per category
 category_lines = []
 all_categories = []
-# suffixes = ['sy/', 'hbj/', 'lyq/', 'yzc/', 'yyk/', 'yyw/', 'swn/', 'lgh/', 'ycy/']
-suffixes = ['sy/', 'hbj/', 'lyq/']
+suffixes = ['sy/', 'hbj/', 'lyq/', 'yzc/', 'yyk/', 'yyw/', 'swn/', 'lgh/', 'ycy/']
+# suffixes = ['sy/', 'hbj/']
 excludes = ['6_np.npy', '4_np.npy']
 
 for suffix in suffixes:
-    rootdir = '../training/sound_final/'+suffix+'combination/'
+    rootdir = '../training/temp/'+suffix
     list = os.listdir(rootdir) #列出文件夹下所有的目录与文件
     for i in range(0, len(list)):
-        if list[i] in excludes or 'swipe' in list[i]:
+        if list[i] in excludes:
             continue
         print(list[i])
         category = list[i].split('_')[0]
         all_categories.append(category)
         filename = os.path.join(rootdir,list[i])
         data = read_data(filename)
-        audio_freq_left = abs(fft(data[:, 1000:1000+22050], axis=1))
-        audio_freq_right = abs(fft(data[:, 1000+22050:1000+44100], axis=1))
-        concat_data = np.concatenate((data[:, 0:1000], audio_freq_left[:, 0:1000], audio_freq_right[:, 0:1000]), axis=1)
-        category_lines.append(concat_data) #此时数据data为n*1000的nparray
+        # audio_freq_left = (abs(fft(data[:, 1000:1000+22050], axis=1)))[:, 0:1000]
+        # audio_freq_right = (abs(fft(data[:, 1000+22050:1000+44100], axis=1)))[:, 0:1000]
+        # audio_freq_left = np.sum(audio_freq_left.reshape(-1, 100, 10), axis=2)
+        # audio_freq_right = np.sum(audio_freq_right.reshape(-1, 100, 10), axis=2)
+        
+        ##mfcc
+        # sampling_freq = 44100
+        # fft_size = 22050
+        # audio_left = data[:, 1000:1000+22050]
+        # audio_right = data[:, 1000+22050:1000+44100]
+        # concat_data = np.zeros((audio_left.shape[0], 52+1000))
+        # for i in range(audio_left.shape[0]):
+        #     audio_left[i] = audio_left[i] / np.linalg.norm(audio_left[i])
+        #     audio_right[i] = audio_right[i] / np.linalg.norm(audio_right[i])
+        #     mfcc_left = mfcc(audio_left[i], samplerate=sampling_freq, winlen=0.5, winstep=0.25, nfft=fft_size)
+        #     mfcc_right = mfcc(audio_right[i], samplerate=sampling_freq, winlen=0.5, winstep=0.25, nfft=fft_size)
+        #     concat_data[i][0:26] = mfcc_left
+        #     concat_data[i][26:52] = mfcc_right
+        #     concat_data[i][52:52+1000] = data[i][0:1000]
+        #     # plt.plot(concat_data[i])
 
-# pca_before = np.concatenate((category_lines))
+        # concat_data = np.concatenate((data[:, 0:1000], audio_freq_left, audio_freq_right), axis=1)
+        # plt.show()
+        category_lines.append(data[:, 0:1000]) #此时数据data为n*(52+1000)的nparray
+
+pca_before = np.concatenate((category_lines))
 # print(pca_before.shape)
 # pca = PCA(n_components=100)
 # pca_after =  pca.fit_transform(pca_before)
 # print(pca_after.shape)
+pca_after = pca_before
+
+print(pca_after.shape)
 
 n_categories = int(len(all_categories) / len(suffixes))
 print(n_categories, len(all_categories), all_categories)
@@ -53,14 +77,6 @@ for i in range(len(all_categories)):
     flag = np.ones(category_lines[i].shape[0]) * (i%n_categories)
     flag_lines.append(flag)
 
-feature_set = np.concatenate((category_lines))
+feature_set = pca_after
 flag_set = np.concatenate((flag_lines))
-
-test_length = np.sum([category_lines[i].shape[0] for i in range(n_categories)])
-print(test_length)
-
-feature_train_set = feature_set[test_length:]
-flag_train_set = flag_set[test_length:]
-feature_test_set = feature_set[:test_length] 
-flag_test_set = flag_set[:test_length]
-print(feature_train_set.shape, flag_train_set.shape, feature_test_set.shape, flag_test_set.shape)
+print(flag_set)
--- lstm-classification/train.py
+++ lstm-classification/train.py
@@ -2,6 +2,7 @@ import torch
 from torch import nn
 from data import *
 from sklearn.model_selection import train_test_split, ShuffleSplit
+from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
 import time
 import matplotlib.pyplot as plt
 
@@ -12,98 +13,134 @@ print('categories: ', all_categories)
 n_seqlen = 50
 n_inputsize = 20
 n_hidden = 128
-n_epochs = 2000
-learning_rate = 0.1 # If you set this too high, it might explode. If too low, it might not learn
-n_freq_in = 2000
+n_epochs = 5000
+learning_rate = 0.01 # If you set this too high, it might explode. If too low, it might not learn
+n_freq_in = 200
 n_freq_out = 128
 
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.lstm = nn.LSTM(n_inputsize, n_hidden, batch_first = True)
-        self.fc_freq = nn.Linear(n_freq_in, n_freq_out)
-        self.fc = nn.Linear(n_hidden+n_freq_out, n_categories)
-        self.logsoftmax = nn.LogSoftmax()
+        self.lstm = nn.LSTM(n_inputsize, n_hidden, batch_first = True).cuda()
+        self.fc = nn.Linear(n_hidden, n_categories).cuda()
+        self.logsoftmax = nn.LogSoftmax().cuda()
+        # self.fc1 = nn.Linear(52, 20).cuda()
+        # self.relu = nn.ReLU().cuda()
 
     def forward(self, x):
-        y = x[:, 1000:3000]
-        x = x[:, 0:1000]
+        # y = x[:, 0:52]
+        # x = x[:, 52:52+1000].reshape(-1, 50 ,20)
+    
         x = x.reshape(-1, 50, 20)
-        
-        x = torch.from_numpy(x.astype('float32'))
+        x = torch.from_numpy(x.astype('float32')).cuda()
         x, (hn, cn) = self.lstm(x)
         x = torch.max(x, dim = 1)[0]
+        
+        # y = torch.from_numpy(y.astype('float32')).cuda()
+        # y = self.fc1(y)
+        # y = self.relu(y)
 
-        y = torch.from_numpy(y.astype('float32'))
-        y = self.fc_freq(y)
-        # print(x.shape, y.shape)
-        x = torch.cat((x, y), 1)
+        # x = torch.cat([x, y], 1)
         x = self.fc(x)
+
         x = x.view(-1, n_categories)
         x = self.logsoftmax(x)
         return x
 
-net = Net()
-optimizer = torch.optim.SGD(net.parameters(), lr=learning_rate)
-criterion = nn.NLLLoss()
-# input = torch.randn(3, n_seqlen, n_inputsize)
-# output, (hn, cn) = rnn(input)
-# print(output.size())
-# input的维度：batch，数据的个数，单个数据的长度；
-# output的维度：batch，数据的个数，隐藏层的维度；（在此任务中，应该只取最后一个隐藏层）
-# 还需要一层全相连
+start = 0
+test_segment = [0]
 
-# 切割数据集
-# seed = int(time.time()*10000000) % 19980608
-# X_train, X_test, y_train, y_test = train_test_split(feature_set, flag_set, test_size=0.2, random_state=seed)
-# print('split result shape: ', X_train.shape, X_test.shape, y_train.shape, y_test.shape)
-X_train, X_test, y_train, y_test = feature_train_set, feature_test_set, flag_train_set, flag_test_set
+for suffix_index in range(len(suffixes)):
+    start = start + np.sum([category_lines[suffix_index*n_categories+i].shape[0] for i in range(n_categories)])
+    test_segment.append(start)
 
-valid_result = []
-test_result = []
+print(test_segment)
 
-for epoch in range(n_epochs):
-    #train
-    optimizer.zero_grad()
-    # 每次随机1/10的train样本作为一个epoch
-    seed = int(time.time()*10000000) % 19980608
-    epoch_X_train, epoch_X_test, epoch_y_train, epoch_y_test = train_test_split(X_train, y_train, test_size=0.9, random_state=seed)
-    # 训练
-    output = net(epoch_X_train)
+tot_res_valid = []
+tot_res_test = []
 
-    epoch_y_train = torch.LongTensor(epoch_y_train)
-    loss = criterion(output, epoch_y_train)
-    # print(loss)
-    loss.backward()
-    optimizer.step()
+for suffix_index in range(len(suffixes)):
+    print(suffixes[suffix_index])
 
-    if epoch % 10 == 0:
+    feature_train_set = np.concatenate((feature_set[: test_segment[suffix_index]], feature_set[test_segment[suffix_index+1]: ]))
+    flag_train_set = np.concatenate((flag_set[: test_segment[suffix_index]], flag_set[test_segment[suffix_index+1]: ]))
+    feature_test_set = feature_set[test_segment[suffix_index]: test_segment[suffix_index+1]] 
+    flag_test_set = flag_set[test_segment[suffix_index]: test_segment[suffix_index+1]]
+    print(feature_train_set.shape, flag_train_set.shape, feature_test_set.shape, flag_test_set.shape)
 
-        #验证
-        output = net(X_train)
-        count = 0
-        for i in range(y_train.shape[0]):
-            if y_train[i] == np.argmax(output.data.numpy()[i]):
-                count = count + 1
-        print(count/y_train.shape[0])
-        valid_result.append(count/y_train.shape[0])
-        #测试
-        output = net(X_test)
-        count = 0
-        for i in range(y_test.shape[0]):
-            if y_test[i] == np.argmax(output.data.numpy()[i]):
-                count = count + 1
-        print(count/y_test.shape[0])
-        test_result.append(count/y_test.shape[0])
+    net = Net()
+    optimizer = torch.optim.SGD(net.parameters(), lr=learning_rate)
+    criterion = nn.NLLLoss()
+    # input = torch.randn(3, n_seqlen, n_inputsize)
+    # output, (hn, cn) = rnn(input)
+    # print(output.size())
+    # input的维度：batch，数据的个数，单个数据的长度；
+    # output的维度：batch，数据的个数，隐藏层的维度；（在此任务中，应该只取最后一个隐藏层）
+    # 还需要一层全相连
 
-        if epoch % 100 == 0:
-            print(epoch)
-        #     plt.plot(valid_result)
-        #     plt.plot(test_result)
-        #     plt.show()
+    # 切割数据集
+    # seed = int(time.time()*10000000) % 19980608
+    # X_train, X_test, y_train, y_test = train_test_split(feature_set, flag_set, test_size=0.2, random_state=seed)
+    # print('split result shape: ', X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+    X_train, X_test, y_train, y_test = feature_train_set, feature_test_set, flag_train_set, flag_test_set
 
-plt.plot(valid_result)
-plt.plot(test_result)
-plt.show()
+    valid_result = []
+    test_result = []
 
-torch.save(net, 'lstm-classification.pt')
+    for epoch in range(n_epochs):
+        #train
+        optimizer.zero_grad()
+        # 每次随机1/10的train样本作为一个epoch
+        seed = int(time.time()*10000000) % 19980608
+        epoch_X_train, epoch_X_test, epoch_y_train, epoch_y_test = train_test_split(X_train, y_train, test_size=0.9, random_state=seed)
+        # epoch_X_train = X_train
+        # epoch_y_train = y_train
+        # 训练
+        output = net(epoch_X_train)
+        
+        epoch_y_train = (torch.LongTensor(epoch_y_train)).cuda()
+        loss = criterion(output, epoch_y_train)
+        # print(loss)
+        loss.backward()
+        optimizer.step()
+
+        if epoch % 10 == 0:
+            predict_temp = []
+            label_temp = []
+            #验证
+            output = net(X_train).cpu()
+            for i in range(y_train.shape[0]):
+                if y_train[i] != 8:
+                    predict_temp.append(np.argmax(output.data.numpy()[i]))
+                    label_temp.append(y_train[i])
+            valid_result.append([accuracy_score(label_temp, predict_temp), precision_score(label_temp, predict_temp, average='weighted'), recall_score(label_temp, predict_temp, average='weighted'), f1_score(label_temp, predict_temp, average='weighted')])
+            #测试
+            predict_temp = []
+            label_temp = []
+            output = net(X_test).cpu()
+            for i in range(y_test.shape[0]):
+                if y_test[i] != 8:
+                    predict_temp.append(np.argmax(output.data.numpy()[i]))
+                    label_temp.append(y_test[i])
+            test_result.append([accuracy_score(label_temp, predict_temp), precision_score(label_temp, predict_temp, average='weighted'), recall_score(label_temp, predict_temp, average='weighted'), f1_score(label_temp, predict_temp, average='weighted')])
+
+            if epoch % 100 == 0:
+                print(epoch, valid_result[-1], test_result[-1])
+                # plt.plot(valid_result)
+                # plt.plot(test_result)
+                # plt.show()
+
+    # plt.plot(valid_result)
+    # plt.plot(test_result)
+    # plt.show()
+    valid_result = np.array(valid_result)
+    test_result = np.array(test_result)
+    print(np.max(valid_result, axis=0))
+    print(np.max(test_result, axis=0))
+    tot_res_valid.append(np.max(valid_result, axis=0))
+    tot_res_test.append(np.max(test_result, axis=0))
+    
+    torch.save(net, 'lstm-classification.pt')
+
+print(tot_res_valid)
+print(tot_res_test)
